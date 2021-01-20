fc1153e036ae747c261032dd6869850a37137dee
zygmunman <jmart237@gmail.com>
2021-01-20 17:48:58 +0100

    Vídeo 18: Roles - Menú Rol I

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -16,7 +16,11 @@
     <!-- AdminLTE Skins. Choose a skin from the css/skins
            folder instead of downloading all of them to reduce the load. -->
     <link rel="stylesheet" href="{{asset("assets/$theme/dist/css/skins/_all-skins.min.css")}}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
     @yield("styles")
+
     <link rel="stylesheet" href="{{asset("assets/css/custom.css")}}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -60,6 +64,9 @@
     @yield("scriptsPlugins")
     <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
     <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="{{asset("assets/js/scripts.js")}}"></script>
     <script src="{{asset("assets/js/funciones.js")}}"></script>
     @yield("scripts")
 </body>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 //use App\Http\Controllers\PermisoController;
 
-use App\Http\Controllers\Admin\MenuController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
+use App\Http\Controllers\Admin\RolController;
+use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\PermisoController;
 
 /*
@@ -18,33 +19,30 @@ use App\Http\Controllers\Admin\PermisoController;
 |
 */
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-/*
-//Route::get('admin/sistema/permiso', [PermisoController::class, 'index'])->name('permiso');
 
-//  Route::view('/permiso', 'permiso');
-
-Route::get('permiso/{nombre}', function($nombre){
-    return $nombre;
-})->where('nombre', '[0-9]+')
- ->name('permiso');
-*/
+/**
+ * ¡¡¡¡NOTA MUY IMPORTANTE!!!!: HAY QUE IMPORTAR LA CLASE DEL CONTROLADOR EN LA PARTE DE ARRIBA
+ */
 
 Route::get('/', [InicioController::class, 'index']);
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+    /** RUTAS DE PERMISO */
     Route::get('permiso', [PermisoController::class, 'index'])->name('permiso');
     Route::get('permiso/crear', [PermisoController::class, 'crear'])->name('crear_permiso');
 
     /** RUTAS DEL MENÚ */
-
     Route::get('menu', [MenuController::class, 'index'])->name('menu');
     Route::get('menu/crear', [MenuController::class, 'crear'])->name('crear_menu');
     Route::post('menu', [MenuController::class, 'guardar'])->name('guardar_menu');
     Route::post('menu/guardar-orden', 'MenuController@guardarOrden')->name('guardar_orden');
+
+    /** RUTAS DE ROL */
+    Route::get('rol', [RolController::class, 'index'])->name('rol');
+    Route::get('rol/crear', [RolController::class, 'crear'])->name('crear_rol');
+    Route::post('rol', [RolController::class, 'guardar'])->name('guardar_rol');
+    Route::get('rol/{id}/editar', [RolController::class, 'editar'])->name('editar_rol');
+    Route::put('rol/{id}', [RolController::class, 'actualizar'])->name('actualizar_rol');
+    Route::delete('rol/{id}', [RolController::class, 'eliminar'])->name('eliminar_rol');
 
 });
